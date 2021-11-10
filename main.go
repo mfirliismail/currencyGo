c0b84fd046fd285ee26140a49ade73b49e440a5e
mfirliismail <m.firliismail@gmail.com>
2021-11-10 19:36:05 +0700

    add simple ping pong game using currency

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 	// "time"
 )
 
@@ -34,8 +35,19 @@ func wgWait() {
 
 func main() {
 
-	wgWait()
-	channelStudy()
+	// wgWait()
+	// channelStudy()
+	// bufferedChannel()
+
+	ping := make(chan int)
+	pong := make(chan int)
+
+	go pinger(ping, pong)
+	go ponger(ping, pong)
+
+	pong <- 1
+
+	select {}
 
 }
 
@@ -60,4 +72,43 @@ func channelStudy() {
 	val = <-c
 	fmt.Println(val)
 	fmt.Println(c)
+}
+
+type Car struct {
+	Model string
+	Age   int
+}
+
+func bufferedChannel() {
+	chanel := make(chan *Car, 3)
+
+	go func() {
+		chanel <- &Car{"BMW", 2001}
+		chanel <- &Car{"Daihatsu", 1998}
+		chanel <- &Car{"Honda JH221", 2014}
+		chanel <- &Car{"Pajero 98K1", 2021}
+
+		close(chanel)
+	}()
+
+	for i := range chanel {
+		fmt.Println(i.Model, i.Age)
+	}
+}
+func pinger(pinger <-chan int, ponger chan<- int) {
+	for {
+		<-pinger
+		fmt.Println("ping")
+		time.Sleep(time.Second)
+		ponger <- 1
+	}
+}
+
+func ponger(pinger chan<- int, ponger <-chan int) {
+	for {
+		<-ponger
+		fmt.Println("pong")
+		time.Sleep(time.Second)
+		pinger <- 1
+	}
 }
